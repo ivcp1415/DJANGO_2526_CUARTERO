bce7b32560195fde9f86e7217f2fda8648cd5fbe
ivcp1415 <cuartero.perez4@gmail.com>
2026-03-05 20:03:24 +0100

    tags added

--- user_viewer/users/views.py
+++ user_viewer/users/views.py
@@ -1,7 +1,59 @@
 from django.shortcuts import render
 
-# Create your views here.
-def get_user(request):
+# this method returns student data
+def get_student(request):
+    student_list = {
+        'student1': {
+            'id': 1,
+            'name': 'Pau',
+            'email': 'pau.serra@itic.cat',
+            'rol': 'Delegat'
+        },
+        'student2': {
+            'id': 2,
+            'name': 'Laia',
+            'email': 'laia.martinez@itic.cat',
+            'rol': 'Subdelegada'
+        },
+        'student3': {
+            'id': 3,
+            'name': 'Marc',
+            'email': 'marc.font@itic.cat',
+            'rol': 'Estudiant'
+        },
+        'student4': {
+            'id': 4,
+            'name': 'Júlia',
+            'email': 'julia.vila@itic.cat',
+            'rol': 'Estudiant'
+        }
+    }
+    context = {'msg': student_list}
+    return render(request, 'students.html', context)
 
-    context = {"msg": "hola"}
-    return render(request, 'users.html', context)
+def get_teacher(request):
+    teacher_list = {
+        'teacher1': {
+            'id': 101,
+            'name': 'Marta',
+            'email': 'marta.rodriguez@itic.cat',
+            'department': 'Matemàtiques',
+            'is_tutor': True
+        },
+        'teacher2': {
+            'id': 102,
+            'name': 'Albert',
+            'email': 'albert.ferrer@itic.cat',
+            'department': 'Informàtica',
+            'is_tutor': False
+        },
+        'teacher3': {
+            'id': 103,
+            'name': 'Sílvia',
+            'email': 'silvia.puig@itic.cat',
+            'department': 'Física',
+            'is_tutor': True
+        }
+    }
+    context = {"msg": teacher_list}
+    return render(request, '', context)
