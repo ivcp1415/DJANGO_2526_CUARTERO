433d4ad657aad954d331609fe3358823dff93d32
ivcp1415 <cuartero.perez4@gmail.com>
2026-03-08 23:42:00 +0100

    Static view

--- user_viewer/users/views.py
+++ user_viewer/users/views.py
@@ -7,25 +7,29 @@ def get_student(request):
             'id': 1,
             'name': 'Pau',
             'email': 'pau.serra@itic.cat',
-            'rol': 'Delegat'
+            'rol': 'Delegat',
+            'mean': 9.2
         },
         'student2': {
             'id': 2,
             'name': 'Laia',
             'email': 'laia.martinez@itic.cat',
-            'rol': 'Subdelegada'
+            'rol': 'Subdelegada',
+            'mean': '9.4'
         },
         'student3': {
             'id': 3,
             'name': 'Marc',
             'email': 'marc.font@itic.cat',
-            'rol': 'Estudiant'
+            'rol': 'Estudiant',
+            'mean': '9.5'
         },
         'student4': {
             'id': 4,
             'name': 'Júlia',
             'email': 'julia.vila@itic.cat',
-            'rol': 'Estudiant'
+            'rol': 'Estudiant',
+            'mean': 9.0
         }
     }
     context = {'msg': student_list}
@@ -53,7 +57,14 @@ def get_teacher(request):
             'email': 'silvia.puig@itic.cat',
             'department': 'Física',
             'is_tutor': True
+        },
+        'teacher4': {
+            'id': 104,
+            'name': 'Marta',
+            'email': 'marta.tarrells@itic.cat',
+            'department': 'Llengua Catalana',
+            'is_tutor': False
         }
     }
     context = {"msg": teacher_list}
-    return render(request, '', context)
+    return render(request, 'teachers.html', context)
