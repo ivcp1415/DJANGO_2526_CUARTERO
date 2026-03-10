798eaf69a5a414a8c4f9157b89cfeb9dfd13aa0b
ivcp1415 <cuartero.perez4@gmail.com>
2026-03-10 18:43:59 +0100

    parameters finished

--- user_viewer/users/urls.py
+++ user_viewer/users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get_students/', views.get_students),
-    path('get_teachers/', views.get_teachers)
+    path('get_students/', views.get_students, name='students'),
+    path('get_teachers/', views.get_teachers, name='teachers'),
+    path('student/<int:pk>/', views.get_student_by_id, name='student'),
+    path('teacher/<int:pk>/', views.get_teacher_by_id, name='teacher')
 ]
